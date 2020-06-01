96b117cc75ccc1cb7a2f6ed9f81720ba9e5ff705
Saskia van Barthold <saskia.vanb@gmail.com>
2020-06-01 20:50:50 +0200

    updated course file with dictionary attempt

--- monster_inc_university/course.py
+++ monster_inc_university/course.py
@@ -8,3 +8,10 @@ class Course():
 
     def start_date(self):
         return "3rd September 2020"
+
+
+
+# course_info = Course(module_name='how to scare 101', list_of_students="James P. Sullivan, Randall Boggs, Mike Wazoski, Roz", start_date="3rd September 2020")
+#
+# course_dic={'module_name', 'list_of_students', 'start_date'}
+# course_dic['modle_name']=module_name
